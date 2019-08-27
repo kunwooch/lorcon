e0145f2ba41bcb9dc2d81c85d856c84668a666ef
kwcho <kwcho@princeton.edu>
2019-08-27 00:56:20 -0400

    trying to fix error

--- tools/injector.c
+++ tools/injector.c
@@ -97,12 +97,6 @@ int GI = 0;
 uint8_t *dmac = "\x04\xF0\x21\x32\xBD\xA5";    
 uint8_t *bmac = "\x00\xDE\xAD\xBE\xEF\x00"; 
 uint8_t RA_MAC[6];  
-RA_MAC[0] = 0x04;
-RA_MAC[1] =0xF0; 
-RA_MAC[2] =0x21;  
-RA_MAC[3] =0x32;
-RA_MAC[4] =0xBD;
-RA_MAC[5] =0xA5; 
 uint8_t *TA_MAC;  
 uint8_t *DA_MAC = RA_MAC;
 uint8_t *BSSID_MAC = bmac;
@@ -375,13 +369,13 @@ void *inject_data(void *_args){
     uint8_t *dmac = "\x04\xF0\x21\x32\xBD\xA5";
     uint8_t *bmac = "\x00\xDE\xAD\xBE\xEF\x00";
 
-    uint8_t RA_MAC[6];
+    uint8_t RA_MAC[6];*/
     RA_MAC[0] = 0x04;
     RA_MAC[1] =0xF0;
     RA_MAC[2] =0x21;
     RA_MAC[3] =0x32;
     RA_MAC[4] =0xBD;
-    RA_MAC[5] =0xA5;
+    RA_MAC[5] =0xA5;/*
     uint8_t *TA_MAC;
     uint8_t *DA_MAC = RA_MAC;
     uint8_t *BSSID_MAC = bmac;
@@ -501,13 +495,13 @@ int main(int argc, char *argv[]) {
     uint8_t *dmac = "\x04\xF0\x21\x32\xBD\xA5";
     uint8_t *bmac = "\x00\xDE\xAD\xBE\xEF\x00";
 
-    uint8_t RA_MAC[6];
+    uint8_t RA_MAC[6];*/
     RA_MAC[0] = 0x04;
     RA_MAC[1] =0xF0;
     RA_MAC[2] =0x21;
     RA_MAC[3] =0x32;
     RA_MAC[4] =0xBD;
-    RA_MAC[5] =0xA5;
+    RA_MAC[5] =0xA5;/*
     uint8_t *TA_MAC;
     uint8_t *DA_MAC = RA_MAC;
     uint8_t *BSSID_MAC = bmac;
